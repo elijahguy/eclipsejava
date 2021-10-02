1bdb2e9b337efbbdd035139ce6754c3c6225b234
Elijah Guy <87336860+elijahguy@users.noreply.github.com>
2021-10-02 11:15:12 -0400

    Add files via upload

---
+++ Class23.java
@@ -0,0 +1,62 @@
+package workplace;
+
+public class Class23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First time using Eclipse..");
+
+
+
+    // 1. Add, subtract, multiply, and divide (quotient & remainder) two numbers and display the output
+   int num1 =2;
+   int num2 = 3;
+
+   int addSum = num1 + num2;
+   int subSum = num1 - num2;
+   int mulSum = num1 * num2;
+   float divSum = num1 / num2;
+
+   System.out.println(num1 +" + " + num2 + " = " + addSum);
+   System.out.println(num1 +" - " + num2 + " = " + subSum);
+   System.out.println(num1 +" * " + num2 + " = " + mulSum);
+   System.out.println(num1 +" / " + num2 + " = " + divSum);
+   
+   // 2. Swap two numbers : Store two numbers in separate variables, display their
+   // initial values, swap them, and display them again.
+   
+   // 3. Display the average of five numbers: Create five separate variables
+   // holding diffrent numbers and find the average 
+   
+   int avgNum1 = 9;
+   int avgNum2 = 1;
+   int avgNum3 = 8;
+   int avgNum4 = 7;
+   int avgNum5 = 6;
+   
+   int average = ((avgNum1 + avgNum2 + avgNum3 + avgNum4 + avgNum5) / 5);
+   
+   System.out.println("The average of" + avgNum1 + "," + avgNum2 + ","+ avgNum3 + "," + avgNum4 + ","+ avgNum5 + " is "+ average);
+   
+   // 4. Write a java program that display the large of two integers. Store these 
+   //numbers in variables.
+   
+   //5. Display the largest of three numbers.
+   
+   //6. Determine if a number is negative, positive, or zero.
+   
+   //7. Determine if a number is even or odd.
+   
+   
+  if (num2 % 2 == 0) { 
+	  System.out.println(num2 + " is even ");
+	  
+	  
+  }
+}
+
+
+	}
+
+
+
---
+++ Class3.java
@@ -0,0 +1,5 @@
+package workplace;
+
+public class Class3 {
+
+}
